1f4d6f42d15c76806b0ec624e120a6b83aee0156
TA <tuyetanh82198@gmail.com>
2024-09-20 14:55:59 +0700

    Refactor

--- routes/cart.js
+++ routes/cart.js
@@ -6,6 +6,8 @@ const {
   getCart,
   deleteCart,
   checkout,
+  checkPayment,
+  getHistory,
 } = require("../controllers/cart");
 const isAuth = require("../middleware/isAuth");
 
@@ -15,5 +17,7 @@ route.post(CART_PATHS.ADD, isAuth, addToCart);
 route.post(CART_PATHS.GET, isAuth, getCart);
 route.post(CART_PATHS.DELETE, isAuth, deleteCart);
 route.post(CART_PATHS.CHECKOUT, isAuth, checkout);
+route.post(CART_PATHS.CHECK_PAYMENT, isAuth, checkPayment);
+route.post(CART_PATHS.HISTORY, isAuth, getHistory);
 
 module.exports = route;
