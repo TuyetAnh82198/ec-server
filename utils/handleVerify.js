4f1eac4ac27f1ec9397d1188505b249dfb5109e3
TA <tuyetanh82198@gmail.com>
2024-09-22 17:13:28 +0700

    Refactor

--- utils/handleVerify.js
+++ utils/handleVerify.js
@@ -5,7 +5,7 @@ const handleVerify = (req) => {
   const cookieUser = req.cookies.user;
   const user = jwt.verify(cookieUser || token, process.env.JWT_SECRET);
   const userId = user._id;
-  return { cookieUser, user, userId };
+  return { user, userId };
 };
 
 module.exports = handleVerify;
